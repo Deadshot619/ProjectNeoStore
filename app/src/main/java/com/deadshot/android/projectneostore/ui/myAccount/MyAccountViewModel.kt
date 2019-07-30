515625f43e9e6570e6758d22b6e4caee07f041c9
Deadshot <pankaj.makhija26@gmail.com>
2019-07-30 15:09:29 +0530

    Add layout to MyAccount Fragment

--- app/src/main/java/com/deadshot/android/projectneostore/ui/myAccount/MyAccountViewModel.kt
+++ app/src/main/java/com/deadshot/android/projectneostore/ui/myAccount/MyAccountViewModel.kt
@@ -1,7 +1,9 @@
 package com.deadshot.android.projectneostore.ui.myAccount
 
 import androidx.lifecycle.ViewModel
+import com.deadshot.android.projectneostore.models.User
 
 class MyAccountViewModel : ViewModel(){
 
+    
 }
