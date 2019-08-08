9fbe220fec6a1c8b6a876910fcea758b8a0777ee
Deadshot <pankaj.makhija26@gmail.com>
2019-08-08 15:35:46 +0530

    Add binding Adapter & new image & animation resources.

---
+++ app/src/main/java/com/deadshot/android/projectneostore/BindingAdapter.kt
@@ -0,0 +1,20 @@
+package com.deadshot.android.projectneostore
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+@BindingAdapter("imgUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?){
+    imgUrl?.let {
+        val imgUri = it.toUri().buildUpon().scheme("http").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image_black_24dp))
+            .into(imgView)
+    }
+}
