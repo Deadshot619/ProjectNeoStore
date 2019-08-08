617371f036607a2b70cfce15a7b7d6566c35cfe8
Deadshot <pankaj.makhija26@gmail.com>
2019-08-08 15:02:49 +0530

    Create Table ViewModel and added it into fragment

--- app/src/main/java/com/deadshot/android/projectneostore/ui/tables/TablesFragment.kt
+++ app/src/main/java/com/deadshot/android/projectneostore/ui/tables/TablesFragment.kt
@@ -6,18 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.deadshot.android.projectneostore.R
+import com.deadshot.android.projectneostore.databinding.FragmentTablesBinding
+import timber.log.Timber
 
 class TablesFragment : Fragment() {
+
+    private lateinit var binding: FragmentTablesBinding
+    private lateinit var tablesViewModel: TablesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //Add Timber
+        Timber.plant(Timber.DebugTree())
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tables, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_tables, container, false)
+
+        //Add Lifecycle owner to this fragment
+        binding.lifecycleOwner = this
+
+
+
+        return binding.root
     }
-
-
 }
---
+++ app/src/main/java/com/deadshot/android/projectneostore/ui/tables/TablesViewModel.kt
@@ -0,0 +1,5 @@
+package com.deadshot.android.projectneostore.ui.tables
+
+import androidx.lifecycle.ViewModel
+
+class TablesViewModel : ViewModel(){}
