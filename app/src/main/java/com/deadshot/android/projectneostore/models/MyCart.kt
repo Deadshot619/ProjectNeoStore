dd7fcb4fbd21d169008e7e0695cc9d93af483859
Deadshot <pankaj.makhija26@gmail.com>
2019-08-20 13:23:15 +0530

    Create Models for MyCart.

---
+++ app/src/main/java/com/deadshot/android/projectneostore/models/MyCart.kt
@@ -0,0 +1,28 @@
+package com.deadshot.android.projectneostore.models
+
+import com.squareup.moshi.Json
+
+data class SingleProductInfo(
+    @Json(name = "cost") val productPrice: Int,
+    @Json(name = "id") val productId: Int,
+    @Json(name = "name") val productName: String,
+    @Json(name = "product_category") val productCategory: String,
+    @Json(name = "product_Images") val productImages: String,
+    @Json(name = "sub_total") val subTotal: Int
+)
+
+data class ProductsInfo(
+    val id: Int,
+    @Json(name = "id") val productId: Int,
+    val quantity: Int,
+    val product: SingleProductInfo
+)
+
+data class MyCartResponse(
+    val status: Int,
+    @Json(name = "data") val productsInfo: List<ProductsInfo>?,
+    val count: Int?,
+    val total: Float?,
+    val message: String?,
+    val user_msg: String?
+)
