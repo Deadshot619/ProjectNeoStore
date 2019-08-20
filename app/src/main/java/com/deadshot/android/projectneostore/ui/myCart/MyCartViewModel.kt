7cec39e5b721d67e77b74204cb1f0ba4980b9250
Deadshot <pankaj.makhija26@gmail.com>
2019-08-20 15:24:08 +0530

    Add Coroutinescope to MyCartViewModel

--- app/src/main/java/com/deadshot/android/projectneostore/ui/myCart/MyCartViewModel.kt
+++ app/src/main/java/com/deadshot/android/projectneostore/ui/myCart/MyCartViewModel.kt
@@ -1,7 +1,31 @@
 package com.deadshot.android.projectneostore.ui.myCart
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 
 class MyCartViewModel : ViewModel(){
+    /**
+     * Create an instance of MyCartRepository
+     */
+//    private val myCartRepository = MyCartRepository()
 
+
+    private var viewModelJob = Job()
+
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    init {
+
+    }
+
+    /**
+     * When the [ViewModel] is finished, we cancel our coroutine [viewModelJob], which tells the
+     * Retrofit service to stop.
+     */
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }
