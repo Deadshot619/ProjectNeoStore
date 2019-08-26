f8779914f6fe31ddab9e68da33ef0b31e2f989e4
Deadshot <pankaj.makhija26@gmail.com>
2019-08-26 16:45:05 +0530

    Add OrderDetail Model.

---
+++ app/src/main/java/com/deadshot/android/projectneostore/models/OrderDetail.kt
@@ -0,0 +1,28 @@
+package com.deadshot.android.projectneostore.models
+
+import com.squareup.moshi.Json
+
+data class ProductsInOrder(
+    val id: Int,
+    @Json(name = "order_id") val orderId: Int,
+    @Json(name = "prod_cat_name") val productCategory: String,
+    @Json(name = "prod_image") val productImage: String,
+    @Json(name = "prod_name") val productName: String,
+    @Json(name = "product_id")val productId: Int,
+    val quantity: Int,
+    val total: Int
+)
+
+data class OrderDetail(
+    @Json(name = "cost") val price: Double ,
+    val address: String ,
+    val id: Int ,
+    @Json(name = "order_details") val ProductsInOrder: List<ProductsInOrder>
+)
+
+data class OrderDetailResponse(
+    val status: Int,
+    @Json(name = "data") val orderDetail: List<OrderDetail>?,
+    val message: String?,
+    val user_msg: String?
+)
