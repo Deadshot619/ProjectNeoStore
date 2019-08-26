0b639989f98bdbb1dd67de52a964130914e449ec
Deadshot <pankaj.makhija26@gmail.com>
2019-08-26 13:05:55 +0530

    Add OrderList() in OrderRepo.
    - add ViewModel Scope in MyOrderViewModel

--- app/src/main/java/com/deadshot/android/projectneostore/repository/OrderRepository.kt
+++ app/src/main/java/com/deadshot/android/projectneostore/repository/OrderRepository.kt
@@ -2,6 +2,9 @@ package com.deadshot.android.projectneostore.repository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.deadshot.android.projectneostore.models.OrderList
+import com.deadshot.android.projectneostore.models.ProductsInfo
+import com.deadshot.android.projectneostore.network.OrderListApi
 import com.deadshot.android.projectneostore.network.OrderNowApi
 import com.deadshot.android.projectneostore.ui.AuthListener
 import kotlinx.coroutines.Dispatchers
@@ -14,6 +17,10 @@ class OrderRepository (private val access_token: String){
     private val _status = MutableLiveData<Boolean>()
     val status: LiveData<Boolean>
         get() = _status
+
+    private val _propertiesOrderList = MutableLiveData<List<OrderList>>()
+    val propertiesOrderList: LiveData<List<OrderList>>
+        get() = _propertiesOrderList
 
     suspend fun placeOrder(access_token: String, address: String){
         withContext(Dispatchers.Main){
@@ -30,6 +37,30 @@ class OrderRepository (private val access_token: String){
                     _status.value = false
                 }
             }catch (t: Throwable){
+                authListener.value?.onFailure("Failure : ${t.message}")
+                Timber.i("Failure : ${t.message}")
+                _status.value = false
+            }
+        }
+    }
+
+    suspend fun orderList(){
+        withContext(Dispatchers.Main){
+            val getPropertiesDeferred = OrderListApi.retrofitService.getOrderList(access_token = access_token)
+
+            try {
+                val listResult = getPropertiesDeferred.await()
+                if (listResult.status == 200){
+                    _propertiesOrderList.value = listResult.orderList
+                    authListener.value?.onSuccess("${listResult.user_msg}")
+                    _status.value = true
+                }else{
+                    _propertiesOrderList.value = null
+                    authListener.value?.onFailure("Error ${listResult.status} : ${listResult.user_msg}")
+                    _status.value = false
+                }
+            }catch (t: Throwable){
+                _propertiesOrderList.value = null
                 authListener.value?.onFailure("Failure : ${t.message}")
                 Timber.i("Failure : ${t.message}")
                 _status.value = false
--- app/src/main/java/com/deadshot/android/projectneostore/ui/myOrders/MyOrdersViewModel.kt
+++ app/src/main/java/com/deadshot/android/projectneostore/ui/myOrders/MyOrdersViewModel.kt
@@ -1,7 +1,45 @@
 package com.deadshot.android.projectneostore.ui.myOrders
 
 import androidx.lifecycle.ViewModel
+import com.deadshot.android.projectneostore.repository.OrderRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MyOrdersViewModel(accessToken: String) : ViewModel(){
+    /**
+     * Lazily Create an instance of [OrderRepository]
+     */
+    private val orderRepository by lazy {
+        OrderRepository(access_token = accessToken)
+    }
 
+    val authListener = orderRepository.authListener
+    val status = orderRepository.status
+
+    val propertiesOrderList = orderRepository.propertiesOrderList
+
+    private var viewModelJob = Job()
+    private var coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    init {
+        loadOrderList()
+    }
+
+    private fun loadOrderList() {
+        coroutineScope.launch {
+            orderRepository.orderList()
+        }
+    }
+
+
+    /**
+     * When the [ViewModel] is finished, we cancel our coroutine [viewModelJob], which tells the
+     * Retrofit service to stop.
+     */
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }
