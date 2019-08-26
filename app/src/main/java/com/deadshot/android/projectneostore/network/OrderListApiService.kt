92c1a058e18e0c7d64c744c555c89957c69b2ff8
Deadshot <pankaj.makhija26@gmail.com>
2019-08-26 12:50:17 +0530

    Add OrderListApiService.

---
+++ app/src/main/java/com/deadshot/android/projectneostore/network/OrderListApiService.kt
@@ -0,0 +1,25 @@
+package com.deadshot.android.projectneostore.network
+
+import com.deadshot.android.projectneostore.models.OrderListResponse
+import kotlinx.coroutines.Deferred
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+/**
+ * A public interface that exposes the [getOrderList] method
+ */
+interface OrderListApiService {
+    @GET("orderList")
+    fun getOrderList(
+        @Header("access_token") access_token: String
+    ): Deferred<OrderListResponse>
+}
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object OrderListApi{
+    val retrofitService: OrderListApiService by lazy {
+        retrofit_moshi.create(OrderListApiService::class.java)
+    }
+}
