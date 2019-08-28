4e7ea92b3163b3589098f259f5eeb97f45182b6b
Deadshot <pankaj.makhija26@gmail.com>
2019-08-28 11:27:30 +0530

    Implement BaseFragment in OrderDetailFragment.

--- app/src/main/java/com/deadshot/android/projectneostore/ui/orderDetail/OrderDetailFragment.kt
+++ app/src/main/java/com/deadshot/android/projectneostore/ui/orderDetail/OrderDetailFragment.kt
@@ -16,6 +16,7 @@ import com.deadshot.android.projectneostore.R
 import com.deadshot.android.projectneostore.adapter.OrderDetailAdapter
 import com.deadshot.android.projectneostore.databinding.FragmentOrderDetailBinding
 import com.deadshot.android.projectneostore.ui.AuthListener
+import com.deadshot.android.projectneostore.ui.BaseFragment
 import com.deadshot.android.projectneostore.utils.ACCESS_TOKEN
 import com.deadshot.android.projectneostore.utils.SHARED_PREFERENCE
 import com.deadshot.android.projectneostore.utils.toastShort
@@ -24,22 +25,21 @@ import timber.log.Timber
 /**
  * A simple [Fragment] subclass.
  */
-class OrderDetailFragment : Fragment(), AuthListener {
+class OrderDetailFragment : BaseFragment() {
 
     private lateinit var orderDetailViewModel: OrderDetailViewModel
     private lateinit var orderDetailModelFactory: OrderDetailModelFactory
-    private lateinit var access_token: String
 
     override fun onCreateView(
         inflater: LayoutInflater , container: ViewGroup? ,
         savedInstanceState: Bundle?
     ): View? {
-        Timber.plant(Timber.DebugTree())
+//        Timber.plant(Timber.DebugTree())
         // Inflate the layout for this fragment
         val binding = FragmentOrderDetailBinding.inflate(inflater)
 
         //load data from shared preferences
-        loadData()
+        loadAccessToken()
 
         //set lifecyle owner
         binding.lifecycleOwner = this
@@ -47,7 +47,7 @@ class OrderDetailFragment : Fragment(), AuthListener {
         //get arguments from bundle passed from calling Fragment
         val arguments= OrderDetailFragmentArgs.fromBundle(arguments!!)
 
-        orderDetailModelFactory = OrderDetailModelFactory(access_token, arguments.orderId)
+        orderDetailModelFactory = OrderDetailModelFactory(accessToken, arguments.orderId)
         orderDetailViewModel =  ViewModelProviders.of(this , orderDetailModelFactory).get(OrderDetailViewModel::class.java)
 
         binding.orderDetailViewModel = orderDetailViewModel
@@ -60,25 +60,4 @@ class OrderDetailFragment : Fragment(), AuthListener {
         orderDetailViewModel.authListener.value = this
         return binding.root
     }
-
-    /**
-     * Load data from shared preferences
-     */
-    private fun loadData(){
-        val sharedPreferences = activity?.getSharedPreferences(SHARED_PREFERENCE , Context.MODE_PRIVATE) ?: return
-        access_token = sharedPreferences.getString(ACCESS_TOKEN , getString(R.string.default_value))!!
-    }
-
-    override fun onStarted() {
-        toastShort("Login Started")
-    }
-
-    override fun onSuccess(message: String) {
-        toastShort(message)
-    }
-
-    override fun onFailure(message: String) {
-        toastShort(message)
-    }
-
 }
