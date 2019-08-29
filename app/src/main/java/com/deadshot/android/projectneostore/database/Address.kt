5a004bc8cfb017a04bafb0cfe2abbcac5f455ca4
Deadshot <pankaj.makhija26@gmail.com>
2019-08-29 16:36:16 +0530

    Create Data Entity class Address.

---
+++ app/src/main/java/com/deadshot/android/projectneostore/database/Address.kt
@@ -0,0 +1,29 @@
+package com.deadshot.android.projectneostore.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "address_table")
+data class Address(
+    @PrimaryKey(autoGenerate = true)
+    val addressId: Long = 0L,
+
+    @ColumnInfo(name = "address")
+    val address: String,
+
+    @ColumnInfo(name = "apartment")
+    val apartment: String,
+
+    @ColumnInfo(name = "country")
+    val country: String,
+
+    @ColumnInfo(name = "state")
+    val state: String,
+
+    @ColumnInfo(name = "city")
+    val city: String,
+
+    @ColumnInfo(name = "zip_code")
+    val zipCode: String
+)
